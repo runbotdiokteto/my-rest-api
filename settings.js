bb27c48cbb821c9c24c9cf68b832a8d0b5155d9f
Aulia Rahman <114418374+liaacans@users.noreply.github.com>
2024-03-29 19:46:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Aulia Rahman'// yourname
+global.MONGO_DB_URI = "new999_selection" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "support@link2u.biz.id" //email
+global.email_password = "pkAm4zHbXgAp" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
